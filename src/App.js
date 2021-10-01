933f8c16ba315fc9923f56a97974add779391c55
premvikas <premvikas.v@gmail.com>
2021-10-01 12:42:37 +0530

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
+
+import { Header } from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+
+    return (
+        <>
+            <h1>hello</h1>
+        </>
+    )
+};
